7a849ccb0fd048025964eab9c1c1f33c7752de2f
johnekk <ekokae_j@etna-alternance.net>
2019-12-26 16:50:06 +0100

    CompanY DTO success

--- src/main/java/com/excilys/cdb/dtos/CompanyDTO.java
+++ src/main/java/com/excilys/cdb/dtos/CompanyDTO.java
@@ -2,4 +2,74 @@ package com.excilys.cdb.dtos;
 
 public class CompanyDTO {
 
+	private int id;
+	private String name;
+	
+	private CompanyDTO(CompanyDTOBuilder builder) {
+		this.id = builder.id;
+		this.name = builder.name;
+	}
+	
+	
+	public static class CompanyDTOBuilder {
+		private int id;
+	    private String name;
+	    
+	    public CompanyDTOBuilder() {};
+	    	
+	    public CompanyDTOBuilder setId(int id) {
+	    	this.id = id;
+	    	return this;
+	   	}
+	    
+	    public CompanyDTOBuilder setName(String name) {
+	    	this.name = name;
+	    	return this;
+	    }
+	    	
+	   	public CompanyDTO build() {
+	    	return new CompanyDTO(this);
+	    }
+	 }
+	 
+	
+	public int getId() {
+			return id;
+	 }
+
+	public String getName() {
+			return name;
+	}
+		
+	public String toString() {
+		return "CompanyDTO [idCompanyDTO=" + id + ", nameCompanyDTO=" + name + "]";
+	}
+
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompanyDTO other = (CompanyDTO) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+				return false;
+		return true;
+	}
 }
